aaeeb4cffc44d974d5a0f869f9511fb3f741926d
rhdqors <rhdqors@gmail.com>
2023-11-14 01:08:59 +0900

    refactor: 고객으로부터 입력받는 방문날짜 접근제어자 수정
    주문 등의 다른 클래스에서 사용하기 위함

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -3,7 +3,7 @@ package christmas;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private int date;
+    public static int date;
 
     public void inputDate() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
