37a120dceb8664ee0e304783a01579b697110e37
rhdqors <rhdqors@gmail.com>
2023-11-16 17:15:21 +0900

    refactor: 에러 메세지 출력하기 위한 상속
    사용자 정의 에러 클래스 ErrorException이 생성될 때 ErrorCode에 정의한 메세지들 출력

--- src/main/java/christmas/ErrorCode.java
+++ src/main/java/christmas/ErrorCode.java
@@ -1,10 +1,10 @@
 package christmas;
 
 public enum ErrorCode {
-    // 방문일
+    // 방문 날짜 지정
     VISIT_DATE_MISMATCH("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     // 주문시
-    HAVE_NOT_MENU("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    NOT_MENU("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     PLESE_ENTER_THE_NUMBER("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ORDER_TYPE_ERROR("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     DUPLICATE_MENU("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
