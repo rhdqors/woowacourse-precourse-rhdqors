d0a817c794e0ab8f10dfdd7545a5c61e1cb09803
rhdqors <rhdqors@gmail.com>
2023-11-16 17:13:16 +0900

    refactor: 뱃지별 금액 단위 상수 선언
    event 금액을 담당하는 EventPrice에 뱃지별 금액 단위 입력

--- src/main/java/christmas/EventPrice.java
+++ src/main/java/christmas/EventPrice.java
@@ -6,7 +6,10 @@ public enum EventPrice {
     D_DAY_DISCOUNT(100),
     D_DAY_BASIC_DISCOUNT(1000),
     DAY_DISCOUNT(2023),
-    SPECIAL_DISCOUNT(1000);
+    SPECIAL_DISCOUNT(1000),
+    SANTA_BADGE(20000),
+    TREE_BADGE(10000),
+    STAR_BADGE(5000);
 
     private final int price;
 
