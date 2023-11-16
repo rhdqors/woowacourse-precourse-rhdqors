d300071dee7f7af3f5b5bcd8d768e2a9e968e1fb
rhdqors <rhdqors@gmail.com>
2023-11-16 17:12:13 +0900

    test: 고객이 입력한 날짜 확인
    입력한 뭇자가 유효한 숫자가 맞는지 확인

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -7,41 +7,24 @@ public class InputView {
 
     public void inputDate() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        date = Integer.parseInt(input);
+        String inputDate = Console.readLine();
+        validateInputDate(inputDate);
 
-        if(!input.matches("[1-31]+")) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
-
+        date = Integer.parseInt(inputDate);
     }
-
-//    public void inputDate() {
-//        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-//        try {
-//            String input = Console.readLine();
-//            date = Integer.parseInt(input);
-//
-//            if(date < 1 || date > 31) {
-//                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-//                throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-////                throw new ErrorException(ErrorCode.VISIT_DATE_MISMATCH);
-//            }
-//        } catch (NumberFormatException e) {
-//            System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-//            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-////            System.out.println(new ErrorException(ErrorCode.VISIT_DATE_MISMATCH));
-////            throw new ErrorException(ErrorCode.VISIT_DATE_MISMATCH);
-//        }
-//
-//    }
 
     public String inputMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-        String input =  Console.readLine();
+        String inputMenus =  Console.readLine();
         System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
 
-        return input;
+        return inputMenus;
+    }
+
+    private void validateInputDate(String inputDate) {
+        if(!inputDate.matches("([1-9]|[12][0-9]|3[01])")) {
+            throw new ErrorException(ErrorCode.VISIT_DATE_MISMATCH);
+        }
     }
 
 }
